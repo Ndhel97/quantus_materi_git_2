317e154baf64072472ad75114ca1e8446a1ebcf1
Ndhel97 <hariz.dz97@gmail.com>
2021-01-06 18:48:13 +0700

    menambahkan fungsi untuk menghitung luas permukaan kubus dan balok

--- main.py
+++ main.py
@@ -6,4 +6,18 @@ def test_luas_segitiga():
     assert result.hitung(10,5)==25 , 'perhitungan salah'
     assert result.hitung(10,-5)==25 , 'input salah'
 
+def test_luas_kubus():
+    result = luas_kubus_balok.LuasKubus()
+    assert result.hitung_LuasKubus(6)==216, 'perhitungan salah'
+    print("Luas Kubus = ", result.hitung_LuasKubus(6))
+
+def test_luas_balok():
+    result = luas_kubus_balok.LuasBalok()
+    assert result.hitung_LuasBalok(3, 4, 5)==94, 'perhitungan salah'
+    print("Luas Balok = ", result.hitung_LuasBalok(3, 4, 5))
+
 test_luas_segitiga()
+
+#test_kubus_balok 
+test_luas_kubus()
+test_luas_balok()
